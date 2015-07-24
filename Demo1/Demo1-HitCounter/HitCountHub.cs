087de2fc595d1658cc170e88192102a53c15a6da
brady gaster <brady.gaster@gmail.com>
2015-07-23 23:45:44 -0700

    created before state

--- Demo1/Demo1-HitCounter/HitCountHub.cs
+++ Demo1/Demo1-HitCounter/HitCountHub.cs
@@ -13,21 +13,8 @@ namespace Demo1_HitCounter
     {
         static int _hitCount = 0;
 
-        public override Task OnConnected()
-        {
-            _hitCount += 1;
+        // handle the connection
 
-            Clients.All.hitCounted(_hitCount);
-
-            return base.OnConnected();
-        }
-
-        public override Task OnDisconnected(bool stopCalled)
-        {
-            _hitCount -= 1;
-            Clients.All.hitCounted(_hitCount);
-
-            return base.OnDisconnected(stopCalled);
-        }
+        // handle the disconnection
     }
 }
